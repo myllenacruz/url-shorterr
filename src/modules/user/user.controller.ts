edd9bc92697459d7cc0ef43206fe055ab2c0e583
Myllena Cruz <myllena.ocruz@gmail.com>
2025-07-28 14:48:38 -0300

    feat(user): add public decorator to create route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { CreateUserService } from './services/create-user.service';
 import { UserEntity } from '@infrastructure/database/entities/user/user.entity';
 import { CreateUser } from '@modules/user/decorators/swagger-user.decorator';
+import { Public } from '@src/authentication/decorators/public.decorator';
 
 @Controller('user')
 export class UserController {
@@ -11,10 +12,12 @@ export class UserController {
     /**
      * Endpoint to create an user.
      * @param {CreateUserDto} data -The data required to create a new user.
+     * @decorator {@link Public} - Marks the endpoint as publicly accessible, bypassing authentication checks.
      * @decorator {@link CreateUser} - Decorator to define Swagger documentation for create an user.
      * @returns {UserEntity} The created user.
      */
     @Post()
+    @Public()
     @CreateUser()
     public async create(@Body() data: CreateUserDto): Promise<UserEntity> {
         const user = await this.createUserService.execute(data);
