38f2d44385cacf95c0b2e286ef3124a49ed35bb0
Myllena Cruz <myllena.ocruz@gmail.com>
2025-07-28 15:44:27 -0300

    feat(url): add list my urls service test

---
+++ src/modules/url/tests/list-my-urls.service.spec.ts
@@ -0,0 +1,45 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ListMyUrlsService } from '@modules/url/services/list-my-urls.service';
+import { UrlRepository } from '@infrastructure/database/repositories/url/url.repository';
+import { urlEntity } from './mocks/url.mock';
+import { userRequest } from '@src/authentication/tests/mocks/user-request.mock';
+
+describe('ListMyUrlsService', () => {
+    let service: ListMyUrlsService;
+    let urlRepository: UrlRepository;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ListMyUrlsService,
+                {
+                    provide: UrlRepository,
+                    useFactory: () => ({
+                        findAllByUserId: jest.fn().mockResolvedValue([urlEntity])
+                    })
+                }
+            ]
+        }).compile();
+
+        service = module.get<ListMyUrlsService>(ListMyUrlsService);
+        urlRepository = module.get<UrlRepository>(UrlRepository);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('execute', () => {
+        it('should call urlRepository.findAllByUserId with correct paramaters', async () => {
+            await service.execute(userRequest);
+            expect(urlRepository.findAllByUserId).toHaveBeenCalledWith(userRequest.id);
+        });
+
+        it('should return an access token', async () => {
+            const result = await service.execute(userRequest);
+            expect(result).toHaveLength(1);
+            expect(result[0].id).toBeTruthy();
+            expect(result[0].shortCode).toBeTruthy();
+        });
+    });
+});
